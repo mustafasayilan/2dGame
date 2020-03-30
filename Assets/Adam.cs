803de8944b386632c9923eb9bc285674ba37d5c4
mustafasayilan <mustafasayilan@gmail.com>
2020-03-30 22:54:48 +0300

    test

---
+++ Assets/Adam.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adam : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float hiz, yukselisHizi;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = movement.normalized * hiz;
+        transform.Translate(hiz * Time.deltaTime , 0, 0);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
