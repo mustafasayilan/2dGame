56f5e29262c1b7fc798021ddfdea1055f3ee0133
mustafasayilan <mustafasayilan@gmail.com>
2020-03-30 23:17:30 +0300

    insan.c# düzenleme

--- Assets/Adam.cs
+++ Assets/Adam.cs
@@ -7,24 +7,20 @@ public class Adam : MonoBehaviour
     // Start is called before the first frame update
 
     public float hiz, yukselisHizi;
-    private Rigidbody2D rb;
+   
     private Vector2 moveVelocity;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        moveVelocity = movement.normalized * hiz;
+        
         transform.Translate(hiz * Time.deltaTime , Input.GetAxisRaw("Vertical") * Time.fixedDeltaTime, 0);
     }
 
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
-    }
+   
 }
